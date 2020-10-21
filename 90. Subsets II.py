fb82fe88ab26baae4a7de8bdf1664a0b64c18bd5
Peter Hou <65183051+laughouw10@users.noreply.github.com>
2020-10-21 14:33:00 +0800

    Create 90. Subsets II.py

---
+++ 90. Subsets II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        for i in range(2 ** len(nums)):
+            out = []
+            num = i
+            for j in range(len(nums) - 1, -1, -1):
+                if num // (2 ** j) > 0:
+                    num -= 2 ** j
+                    out.append(nums[j])
+            out = sorted(out)
+            if out not in ans:
+                ans.append(out)
+        return ans
+        
