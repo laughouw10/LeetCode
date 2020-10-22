8abbb5da6b5c67e2d560a1f84a045f12ab00b15a
Peter Hou <65183051+laughouw10@users.noreply.github.com>
2020-10-23 02:19:52 +0800

    Create 543. Diameter of Binary Tree.py
    https://leetcode.com/problems/diameter-of-binary-tree/

---
+++ 543. Diameter of Binary Tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 1
+        def find(node):
+            if not node: 
+                return 0
+            left = find(node.left)
+            right = find(node.right)
+            self.ans = max(self.ans, left + right + 1)
+            return max(left, right) + 1
+
+        find(root)
+        return self.ans - 1
