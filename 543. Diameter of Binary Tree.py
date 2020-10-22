dc335c43e63a8ab582c345ab5b78a59459d17891
Peter Hou <65183051+laughouw10@users.noreply.github.com>
2020-10-23 02:25:00 +0800

    Update 543. Diameter of Binary Tree.py

--- 543. Diameter of Binary Tree.py
+++ 543. Diameter of Binary Tree.py
@@ -6,14 +6,14 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        self.ans = 1
+        self.ans = 0
         def find(node):
             if not node: 
                 return 0
             left = find(node.left)
             right = find(node.right)
-            self.ans = max(self.ans, left + right + 1)
+            self.ans = max(self.ans, left + right)
             return max(left, right) + 1
 
         find(root)
-        return self.ans - 1
+        return self.ans
