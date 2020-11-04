d4f159ecf4c7ad1c232aeab1e9fc79fa22b23aca
Peter Hou <65183051+laughouw10@users.noreply.github.com>
2020-11-04 20:58:04 +0800

    Update 90. Subsets II.py

--- 90. Subsets II.py
+++ 90. Subsets II.py
@@ -1,14 +1,19 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        for i in range(2 ** len(nums)):
-            out = []
-            num = i
-            for j in range(len(nums) - 1, -1, -1):
-                if num // (2 ** j) > 0:
-                    num -= 2 ** j
-                    out.append(nums[j])
-            out = sorted(out)
-            if out not in ans:
-                ans.append(out)
+        result = []
+        def helper(nums, result):
+            if nums == []:
+                result = sorted(result)
+                if result not in ans:
+                    ans.append(result.copy())
+            else:
+                num = nums.pop()
+                result.append(num)
+                helper(nums, result)
+                result.pop()
+                helper(nums, result)
+                nums.append(num)
+                
+        helper(nums, result)
         return ans
